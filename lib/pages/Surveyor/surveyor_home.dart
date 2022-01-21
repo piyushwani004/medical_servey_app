e4eadb1e2e646081cca969429eb415033e04cfc0
Piyush wani <piyushwani04@gmail.com>
2022-01-21 19:43:27 +0530

    add home pages

---
+++ lib/pages/Surveyor/surveyor_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SurveyorHomePage extends StatefulWidget {
+  const SurveyorHomePage({Key? key}) : super(key: key);
+
+  @override
+  _SurveyorHomePageState createState() => _SurveyorHomePageState();
+}
+
+class _SurveyorHomePageState extends State<SurveyorHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
