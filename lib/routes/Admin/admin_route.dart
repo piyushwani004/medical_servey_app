e553affd808f48dcdc737de09906323b02535fb3
Piyush wani <piyushwani04@gmail.com>
2022-01-21 11:21:48 +0530

    add routes

---
+++ lib/routes/Admin/admin_route.dart
@@ -0,0 +1,30 @@
+// import 'package:flutter/material.dart';
+
+// const routeLogin = '/login';
+// const routeRegister = '/Register';
+// const routeHome = '/home';
+
+// class Routes {
+//   static Route<dynamic> generateRoute(RouteSettings settings) {
+//     switch (settings.name) {
+//       case routeLogin:
+//         return PageTransition(
+//             type: PageTransitionType.rightToLeft, child: LoginScreen());
+//         break;
+//       case routeRegister:
+//         //Map mapData = settings.arguments;
+//         return PageTransition(
+//             type: PageTransitionType.rightToLeft, child: RegistrationScreen());
+//         break;
+//       case routeHome:
+//         //Map mapData = settings.arguments;
+//         return PageTransition(
+//             type: PageTransitionType.rightToLeft, child: HomeScreen());
+//         break;
+//       default:
+//         return PageTransition(
+//             type: PageTransitionType.rightToLeft, child: LoginScreen());
+//         break;
+//     }
+//   }
+// }
