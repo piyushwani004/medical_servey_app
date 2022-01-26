ec3f7725860581943aa972ad5a6b4ea811734dd9
Rahil Qureshi <53871685+rahilq838@users.noreply.github.com>
2022-01-26 14:00:37 +0530

    surveyor form completed

--- lib/pages/Surveyor/surveyor_home.dart
+++ lib/pages/Surveyor/surveyor_home.dart
@@ -11,6 +11,7 @@ class SurveyorHomePage extends StatefulWidget {
 
 class _SurveyorHomePageState extends State<SurveyorHomePage> {
   var width, height;
+
   @override
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
@@ -43,36 +44,122 @@ class _SurveyorHomePageState extends State<SurveyorHomePage> {
             ],
           ),
 
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: Common.allPadding(mHeight: height),
-              child: Column(
-                children: [
-                  Row(
+          ListView(
+            children: [
+              Align(
+                // name of profile
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: Common.allPadding(mHeight: height),
+                  child: Column(
                     children: [
-                      Padding(
-                        padding: Common.allPadding(mHeight: height),
-                        child: CircleAvatar(
-                          //first letter of name
-                          child: Text('N'),
-                        ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: Common.allPadding(mHeight: height),
+                            child: CircleAvatar(
+                              //first letter of name
+                              child: Text('N'),
+                            ),
+                          ),
+                          Padding(
+                            padding: Common.allPadding(mHeight: height),
+                            child: Column(
+                              children: [Text('Name'), Text('more Info')],
+                            ),
+                          )
+                        ],
                       ),
-                      Padding(
-                        padding: Common.allPadding(mHeight: height),
-                        child: Column(
-                          children: [
-                            Text('Name'),
-                            Text('more Info')
-                          ],
-                        ),
+                    ],
+                  ),
+                ),
+              ),
+              //table containers for each feature
+              Padding(
+                padding: Common.allPadding(mHeight: height * 1),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Table(
+                        children: [
+                          TableRow(children: [
+                            Padding(
+                              padding: Common.allPadding(mHeight: height * 0.9),
+                              child: Container(
+                                decoration: Common.containerBoxDecoration(),
+                                height: 200,
+                                width: 100,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.holiday_village_rounded, size: height*0.09,),
+                                    Text("Select Village")
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: Common.allPadding(mHeight: height * 0.9),
+                              child: Container(
+                                decoration: Common.containerBoxDecoration(),
+                                height: 200,
+                                width: 100,
+                                child:  Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.person_add_rounded, size: height*0.09,),
+                                    Text("Add Patient")
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ]),
+                          TableRow(children: [
+                            Padding(
+                              padding: Common.allPadding(mHeight: height * 0.9),
+                              child: Container(
+                                decoration: Common.containerBoxDecoration(),
+                                height: 200,
+                                width: 100,
+                                child:  Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.edit, size: height*0.09,),
+                                    Text("Update Patient")
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: Common.allPadding(mHeight: height * 0.9),
+                              child: Container(
+                                decoration: Common.containerBoxDecoration(),
+                                height: 200,
+                                width: 100,
+                                child:  Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.person_outline_rounded, size: height*0.09,),
+                                    Text("Surveyor Profile")
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ])
+                        ],
                       )
                     ],
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
+            ],
+          )
         ],
       ),
     );
