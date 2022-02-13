4a0869f5d140d46cfe35b0384bdda5991a251755
Piyush wani <piyushwani04@gmail.com>
2022-02-13 20:14:22 +0530

    add dropdown to report page

--- lib/pages/Admin/generate_report.dart
+++ lib/pages/Admin/generate_report.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
+import 'package:medical_servey_app/models/common/villageData.dart';
 import 'package:medical_servey_app/pages/Admin/main/components/side_menu.dart';
 import 'package:medical_servey_app/utils/constants.dart';
+import 'package:medical_servey_app/utils/image_utils.dart';
 import 'package:medical_servey_app/utils/responsive.dart';
 import 'package:medical_servey_app/widgets/CustomScrollViewBody.dart';
 import 'package:medical_servey_app/widgets/common.dart';
@@ -15,6 +20,34 @@ class GenerateReport extends StatefulWidget {
 
 class _GenerateReportState extends State<GenerateReport> {
   var width, height;
+  List<VillageData> villageData = [];
+  List<String> villages = [];
+  List<String> taluka = [];
+
+  Future<void> fetchDataFromJson() async {
+    final assetBundle = DefaultAssetBundle.of(context);
+    final data = await assetBundle.loadString(JSON_PATH);
+    List body = json.decode(data)['Sheet1'];
+    setState(() {
+      villageData = body.map((e) => VillageData.fromMap(e)).toList();
+      villages = villageData.map((e) => e.village).toSet().toList();
+      taluka = villageData.map((e) => e.taluka).toSet().toList();
+    });
+  }
+
+  onVillageChanged(village) {
+    print("village $village");
+  }
+
+  onTalukaChanged(taluka) {
+    print("village $taluka");
+  }
+
+  @override
+  void initState() {
+    fetchDataFromJson();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,6 +84,46 @@ class _GenerateReportState extends State<GenerateReport> {
   }
 
   Widget body(width, height) {
-    return Container();
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Flexible(
+              child: Padding(
+                padding: Common.allPadding(mHeight: height),
+                child: DropdownSearch<String>(
+                  mode: Mode.DIALOG,
+                  showSearchBox: true,
+                  items: villages,
+                  showSelectedItems: true,
+                  dropdownSearchDecoration: Common.textFormFieldInputDecoration(
+                      labelText: "Select Village"),
+                  onChanged: (saved) => onVillageChanged(saved),
+                  showClearButton: true,
+                  validator: (v) => v == null ? "required field" : null,
+                ),
+              ),
+            ),
+            Flexible(
+              child: Padding(
+                padding: Common.allPadding(mHeight: height),
+                child: DropdownSearch<String>(
+                  mode: Mode.DIALOG,
+                  showSearchBox: true,
+                  items: taluka,
+                  showSelectedItems: true,
+                  dropdownSearchDecoration: Common.textFormFieldInputDecoration(
+                      labelText: "Select Taluka"),
+                  onChanged: (saved) => onTalukaChanged(saved),
+                  showClearButton: true,
+                  validator: (v) => v == null ? "required field" : null,
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
